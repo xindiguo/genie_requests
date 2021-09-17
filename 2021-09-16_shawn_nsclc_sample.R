ad9989a5486a36423d4a3babb7112d62a934f4e7
hhunterzinck <haley.hunterzinck@sagebase.org>
2021-09-17 17:32:38 +0000

    select random sample reproducibly

--- 2021-09-16_shawn_nsclc_sample.R
+++ 2021-09-16_shawn_nsclc_sample.R
@@ -6,7 +6,8 @@
 #     Request date: 2021-09-16
 #     Message:  randomly select 50 patients from the BPC NSCLC 2.1-consortium 
 #              release and generate the 8 release files for just those 50 patients
-
+# Note on usage: remember to do "Session --> Restart R" before running so same 
+#     patient IDs sampled from the stored random seed
 
 # setup ----------------------------
 
@@ -16,13 +17,17 @@ library(glue)
 library(synapser)
 synLogin()
 library(dplyr)
-library(testthat)
 
 # synapse
-synid_file_manifest <- "syn26209101"
-synid_files_data <- c("syn25985885", "syn25985887", "syn25985888")
 synid_folder_sample <- "syn26209100"
 synid_folder_data <- "syn25982471"
+
+# parameters
+n_sample = 50
+
+# random
+seed <- 540827
+set.seed(seed)
 
 # functions ----------------------------
 
@@ -30,8 +35,13 @@ get_synapse_entity_name <- function(synapse_id) {
   return(synGet(synapse_id, downloadFile = F)$properties$name)
 }
 
-
-save_to_synapse <- function(path, parent_id, file_name = NA, prov_name = NA, prov_desc = NA, prov_used = NA, prov_exec = NA) {
+save_to_synapse <- function(path, 
+                            parent_id, 
+                            file_name = NA, 
+                            prov_name = NA, 
+                            prov_desc = NA, 
+                            prov_used = NA, 
+                            prov_exec = NA) {
   
   if (is.na(file_name)) {
     file_name = path
@@ -74,8 +84,6 @@ get_synapse_folder_children <- function(synapse_id) {
 
 # read ----------------------------
 
-patient_ids <- unlist(get_synapse_entity_data_in_csv(synid_file_manifest, header = F))
-
 synid_files_data <- get_synapse_folder_children(synid_folder_data)
 
 datas <- list()
@@ -86,38 +94,28 @@ for (i in 1:length(synid_files_data)) {
 
 # main ----------------------------
 
-sampled <- list()
+# sample 50 IDs randomly for patient level file
+synid_file_patient <- as.character(synid_files_data["patient_level_dataset.csv"])
+patient_ids <- unlist(sample(datas[[synid_file_patient]]$record_id, n_sample))
 
+sampled <- list()
 for (i in 1:length(datas)) {
-  sampled[[names(datas)[i]]] <- datas[[i]] %>% 
+  sampled[[names(datas)[i]]] <- datas[[names(datas)[i]]] %>% 
     filter(is.element(record_id, patient_ids))
 }
 
-# write -----------------------------
-
-# save to synapse
-# for (i in 1:length(sampled)) {
-#   
-#   file_output <- gsub(pattern = ".csv", replacement = "_sample.csv", 
-#                     x = get_synapse_entity_name(names(sampled)[i]))
-#   
-#   write.csv(sampled[[i]], row.names = F, file = file_output)
-#   save_to_synapse(path = file_output, parent_id = synid_folder_sample)
-#   file.remove(file_output)
-# }
-
 # checks --------------------------------
 
-# dimentions
-print(lapply(sampled, dim))
+print(glue("Number of samples in patient list equals number of requested samples: {n_sample == length(patient_ids)}"))
 
 # record_ids
 for (i in 1:length(sampled)) {
   
-  file_name <- get_synapse_entity_name(synapse_id)
   synapse_id <- names(sampled)[i]
+  file_name <- get_synapse_entity_name(synapse_id)
   
   print(glue("File: {file_name} ({synapse_id})"))
+  print(glue("Dimensions of the extract: {nrow(sampled[[i]])} x {ncol(sampled[[i]])}"))
   print(glue("All record IDs are in the manifest: {all(is.element(unlist(sampled[[i]]$record_id), patient_ids))}"))
   print(glue("Number of overlapping records IDs: {length(intersect(unlist(sampled[[i]]$record_id), patient_ids))}"))
   print(glue("Number of missing records IDs: {length(setdiff(patient_ids, unlist(sampled[[i]]$record_id)))}"))
@@ -126,27 +124,33 @@ for (i in 1:length(sampled)) {
   print("-----------")
 }
 
-# check existing sampled files -------------------------
+# write -----------------------------
 
-synid_children <- get_synapse_folder_children(synid_folder_sample)
-for (i in 1:length(synid_children)) {
-  
-  synapse_id <- as.character(synid_children[i])
-  file_name <- get_synapse_entity_name(synapse_id)
-  
-  print(glue("File: {file_name} ({synapse_id})"))
-  if (grepl(pattern = "_sample.csv", x = file_name)) {
-    data <- get_synapse_entity_data_in_csv(synapse_id)
-    
-    
-    print(glue("Number of rows: {nrow(data)}"))
-    print(glue("Number of columns: {ncol(data)}"))
-    print(glue("All record IDs are in the manifest: {all(is.element(unlist(data$record_id), patient_ids))}"))
-    print(glue("Number of overlapping records IDs: {length(intersect(unlist(data$record_id), patient_ids))}"))
-    print("-----------")
-  }
-  
-  
+# # patient IDs to synapse
+file_output <- "patient_ids_sample.csv"
+write(patient_ids, file = file_output, ncolumns = 1)
+save_to_synapse(path = file_output,
+                parent_id = synid_folder_sample,
+                prov_name = "Patient IDs for data sample",
+                prov_desc = "Fifty patient IDs sampled from the GENIE BPC NSCLC 2.1-consortium release",
+                prov_used = synid_file_patient,
+                prov_exec = "https://github.com/hhunterzinck/genie_requests/blob/main/2021-09-16_shawn_nsclc_sample.R")
+file.remove(file_output)
+
+# data sample files to synapse
+for (i in 1:length(sampled)) {
+
+  file_output <- gsub(pattern = ".csv", replacement = "_sample.csv",
+                    x = get_synapse_entity_name(names(sampled)[i]))
+
+  write.csv(sampled[[i]], row.names = F, file = file_output)
+  save_to_synapse(path = file_output,
+                  parent_id = synid_folder_sample,
+                  prov_name = "Data sample",
+                  prov_desc = "Data for fifty sampled patients from the GENIE BPC NSCLC 2.1-consortium release",
+                  prov_used = names(sampled)[i],
+                  prov_exec = "https://github.com/hhunterzinck/genie_requests/blob/main/2021-09-16_shawn_nsclc_sample.R")
+  file.remove(file_output)
 }
 
 # close out ----------------------------
