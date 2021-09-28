ce4a0b4f8f988b1504e35cf4adb0db599c5abdf5
hhunterzinck <haley.hunterzinck@sagebase.org>
2021-09-28 20:00:13 +0000

    remove ovarian sample from clinical file

--- 2021-09-28_sammi_ovarian_sample_in_brca.R
+++ 2021-09-28_sammi_ovarian_sample_in_brca.R
@@ -18,9 +18,11 @@ library(synapser)
 synLogin()
 
 # synapse
+synid_folder_clinical <- "syn26253353"
 synid_file_ca <- "syn26253370"
 synid_file_vicc_brca_upload <- "syn24201984"
-code_ovarian = 'LGSOC'
+code_ovarian <- 'LGSOC'
+sample_id_ovarian <- "GENIE-VICC-508075-unk-1"
 
 # functions ----------------------------
 
@@ -31,12 +33,43 @@ get_synapse_entity_data_in_csv <- function(synapse_id, sep = ",", na.strings = c
   return(data)
 }
 
+save_to_synapse <- function(path, 
+                            parent_id, 
+                            file_name = NA, 
+                            prov_name = NA, 
+                            prov_desc = NA, 
+                            prov_used = NA, 
+                            prov_exec = NA) {
+  
+  if (is.na(file_name)) {
+    file_name = path
+  } 
+  file <- File(path = path, parentId = parent_id, name = file_name)
+  
+  if (!is.na(prov_name) || !is.na(prov_desc) || !is.na(prov_used) || !is.na(prov_exec)) {
+    act <- Activity(name = prov_name,
+                    description = prov_desc,
+                    used = prov_used,
+                    executed = prov_exec)
+    file <- synStore(file, activity = act)
+  } else {
+    file <- synStore(file)
+  }
+  
+  return(T)
+}
+
+get_synapse_entity_name <- function(synapse_id) {
+  return(synGet(synapse_id, downloadFile = F)$properties$name)
+}
+
+
 # read ----------------------------
 
 data_clinical <- get_synapse_entity_data_in_csv(synid_file_ca)
 data_upload <- get_synapse_entity_data_in_csv(synid_file_vicc_brca_upload)
 
-# main ----------------------------
+# check for sample  ----------------------------
 
 data_clinical %>% 
   filter(cpt_oncotree_code == code_ovarian) %>%
@@ -46,6 +79,17 @@ data_upload %>%
   filter(cpt_oncotree_code == code_ovarian) %>%
   select(record_id, cpt_genie_sample_id, cpt_oncotree_code)
 
+# remove sample  ----------------------------
+
+mod <- data_clinical %>% 
+  filter(cpt_oncotree_code != code_ovarian) 
+
+dim(data_clinical)
+dim(mod)
+
+file_output <- get_synapse_entity_name(synid_file_ca)
+write.csv(mod, row.names = F, file = file_output)
+save_to_synapse(file_output, parent_id = synid_folder_clinical)
 
 # close out ----------------------------
 
