8421474cc36412f6017f48f95a74e926ac8a77ef
hhunterzinck <haley.hunterzinck@sagebase.org>
2021-10-28 18:46:25 +0000

    check non-numeric values in upload

---
+++ 2021-10-28_msk_age_non_numeric.R
@@ -0,0 +1,68 @@
+# Description: Get non-numeric values in age in msk prostate upload.
+# Author: Haley Hunter-Zinck
+# Date: 2021-10-28
+
+# setup ----------------------------
+
+tic = as.double(Sys.time())
+
+library(glue)
+library(dplyr)
+library(synapser)
+synLogin()
+
+# synapse
+synid_file_upload <- "syn26230493"
+
+# functions ----------------------------
+
+#' Download and load data stored in csv or other delimited format on Synapse
+#' into an R data frame.
+#' 
+#' @param synapse_id Synapse ID
+#' @version Version of the Synapse entity to download.  NA will load current
+#' version
+#' @param set Delimiter for file
+#' @param na.strings Vector of strings to be read in as NA values
+#' @param header TRUE if the file contains a header row; FALSE otherwise.
+#' @param check_names TRUE if column names should be modified for compatibility 
+#' with R upon reading; FALSE otherwise.
+#' @return data frame
+get_synapse_entity_data_in_csv <- function(synapse_id, 
+                                           version = NA,
+                                           sep = ",", 
+                                           na.strings = c("NA"), 
+                                           header = T,
+                                           check_names = F) {
+  
+  if (is.na(version)) {
+    entity <- synGet(synapse_id)
+  } else {
+    entity <- synGet(synapse_id, version = version)
+  }
+  
+  data <- read.csv(entity$path, stringsAsFactors = F, 
+                   na.strings = na.strings, sep = sep, check.names = check_names,
+                   header = header)
+  return(data)
+}
+
+
+# read ----------------------------
+
+data <- get_synapse_entity_data_in_csv(synid_file_upload, 
+                                       na.strings = c(""))
+
+
+# main ----------------------------
+
+age <- data %>% 
+  select(age_at_seq_report) %>%
+  distinct()
+
+print(age)
+  
+# close out ----------------------------
+
+toc = as.double(Sys.time())
+print(glue("Runtime: {round(toc - tic)} s"))
