952426a3e5fdd77d25822c322b45d5119d752c6c
hhunterzinck <haley.hunterzinck@sagebase.org>
2021-11-01 23:00:21 +0000

    validate with real regimens

--- 2021-11-01_haley_ncit_codes.py
+++ 2021-11-01_haley_ncit_codes.py
@@ -4,29 +4,38 @@ import pandas as pd
 syn = synapseclient.Synapse()
 syn.login()
 
-def _get_synid_dd(cohort, synid_table_prissmm = "syn22684834"):
+def _get_synid_dd(syn, cohort, synid_table_prissmm = "syn22684834"):
+    """Get Synapse ID of the most current PRISSMM non-PHI data dictionary for the BPC cohort."""
 
     query = 'SELECT id FROM ' + synid_table_prissmm + ' WHERE cohort = \'' + cohort + '\' ORDER BY name DESC LIMIT 1'
     query_results = syn.tableQuery(query)
-
+    
     synid_folder_prissmm = query_results.asDataFrame()['id'][0]
-
+    
     synid_prissmm_children = syn.getChildren(synid_folder_prissmm)
-
+    
     for child in synid_prissmm_children:
         if child['name'] == "Data Dictionary non-PHI":
             return child['id'] 
     return None
 
-def get_drug_mapping(cohort, synid_file_grs = 'syn24184523'):
-
+def get_drug_mapping(syn, cohort, synid_file_grs = 'syn24184523'):
+    """
+    Get a mapping between drug short names and NCIT code from BPC data dictionary
+    and BPC global response set for a given BPC cohort.
+    
+    Returns:
+      dictionary: map where keys are BPC drug short names and value is the 
+        corresponding NCIT drug code
+    """
+    
     mapping = {}
     var_names = []
 
-    synid_file_dd = _get_synid_dd(cohort)
+    synid_file_dd = _get_synid_dd(syn, cohort)
 
-    dd = pd.read_csv(syn.get(synid_file_dd).path)
-    grs = pd.read_csv(syn.get(synid_file_grs).path)
+    dd = pd.read_csv(syn.get(synid_file_dd).path, encoding = 'unicode_escape')
+    grs = pd.read_csv(syn.get(synid_file_grs).path, encoding = 'unicode_escape')
     grs.columns = ['Variable / Field Name', 'Choices, Calculations, OR Slider Labels']
 
     for i in ['1','2','3','4','5']:
@@ -48,19 +57,64 @@ def get_drug_mapping(cohort, synid_file_grs = 'syn24184523'):
                     mapping[label] = code
     return(mapping)
 
-def get_regimen_abbrev(regimen, mapping):
-    abbrev = ''
+def get_regimen_abbr(regimen, mapping):
+    """ 
+    Given a BPC regimen and mapping between drug names and NCIT codes,
+    return the regimen abbreviation consisting of NCIT codes
+    """
+    abbr = ''
     drugs = regimen.split(',')
     for drug in drugs:
         if drug == drugs[0]:
-            abbrev = mapping[drug.strip()]
+            abbr = mapping[drug.strip()]
         else:
-            abbrev = abbrev + '_' + mapping[drug.strip()]
-    return(abbrev)
+            abbr = abbr + '_' + mapping[drug.strip()]
+    return(abbr)
 
+# parameters
+synid_file_drug = 'syn22296818'
 cohort = 'Prostate'
-regimen = 'ARV110, CC94676, Vecabrutinib'
-mapping = get_drug_mapping(cohort)
-abbrev = get_regimen_abbrev(regimen, mapping)
+abbrs = []
+top_x_regimens=100
 
-print(abbrev)
+# get regimen abbreviations
+mapping = get_drug_mapping(syn, cohort)
+# mapping of drug names to NCIT codes
+regimen_synid = "syn22296818"
+regimens_to_exclude = ["Investigational Drug"]
+regimen_ent = syn.get(regimen_synid)
+regimendf = pd.read_csv(regimen_ent.path)
+# Get only NSCLC cohort
+regimendf = regimendf[regimendf['cohort'] == cohort]
+# Use redcap_ca_index == Yes
+regimendf = regimendf[regimendf['redcap_ca_index'] == "Yes"]
+# Exclude regimens
+regimendf = regimendf[~regimendf['regimen_drugs'].isin(regimens_to_exclude)]
+regimendf = regimendf[
+    ~regimendf['regimen_drugs'].str.contains("Investigational Drug")
+]
+# Exclude all regimens with "Other"
+regimendf = regimendf[~regimendf['regimen_drugs'].str.contains("Other")]
+# sort file by regimen_number and drop rest of duplicates
+# (not all duplicates), if duplicated keep the first regimen
+regimendf.sort_values('regimen_number', inplace=True)
+regimendf.drop_duplicates(["record_id", "regimen_drugs"], inplace=True)
+
+count_of_regimens = regimendf['regimen_drugs'].value_counts()
+# Obtain top X number of regimens
+to_include_regimens = count_of_regimens[:top_x_regimens].index.tolist()
+
+subset_regimendf = regimendf[
+    regimendf['regimen_drugs'].isin(to_include_regimens)
+]
+regimen_groups = subset_regimendf.groupby("regimen_drugs")
+new_regimen_info = pd.DataFrame()
+# Create regimen clinical headers
+final_regimendf = pd.DataFrame()
+for regimen, df in regimen_groups:
+    # Create regimen drug abbreviations
+    abbrs.append(get_regimen_abbr(regimen, mapping))
+
+
+print(abbrs)
+
