2902827619712ef17965c5cb9037b47a4d41d7e2
hhunterzinck <haley.hunterzinck@sagebase.org>
2021-11-15 18:47:38 +0000

    check case selection lists

---
+++ 2021-11-15_haley_case_list_mult_sample.R
@@ -0,0 +1,63 @@
+# Description: Check if case selection lists for NSCLC phase 2 have patients represented multiple times
+#     because of multiple samples. 
+# Author: Haley Hunter-Zinck
+# Date: 2021-11-15
+
+# setup ----------------------------
+
+tic = as.double(Sys.time())
+
+library(glue)
+library(dplyr)
+library(synapser)
+synLogin()
+
+# synapse
+synid_file_dfci <- "syn26433298"
+
+# functions ----------------------------
+
+#' Download and load data stored in csv or other delimited format on Synapse
+#' into an R data frame.
+#' 
+#' @param synapse_id Synapse ID
+#' @version Version of the Synapse entity to download.  NA will load current
+#' version
+#' @param set Delimiter for file
+#' @param na.strings Vector of strings to be read in as NA values
+#' @param header TRUE if the file contains a header row; FALSE otherwise.
+#' @param check_names TRUE if column names should be modified for compatibility 
+#' with R upon reading; FALSE otherwise.
+#' @return data frame
+get_synapse_entity_data_in_csv <- function(synapse_id, 
+                                           version = NA,
+                                           sep = ",", 
+                                           na.strings = c("NA"), 
+                                           header = T,
+                                           check_names = F) {
+  
+  if (is.na(version)) {
+    entity <- synGet(synapse_id)
+  } else {
+    entity <- synGet(synapse_id, version = version)
+  }
+  
+  data <- read.csv(entity$path, stringsAsFactors = F, 
+                   na.strings = na.strings, sep = sep, check.names = check_names,
+                   header = header)
+  return(data)
+}
+
+# read ----------------------------
+
+data <- get_synapse_entity_data_in_csv(synid_file_dfci)
+
+# main ----------------------------
+
+length(data$PATIENT_ID)
+length(unique(data$PATIENT_ID))
+
+# close out ----------------------------
+
+toc = as.double(Sys.time())
+print(glue("Runtime: {round(toc - tic)} s"))
